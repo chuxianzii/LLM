3ea1558f80f9bd8118cc35ac2799d68cf15fee44
chuxianzii <330670120@qq.com>
2025-05-12 20:58:01 +0800

    初步实现chat功能

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 学习记录
+## 配置环境
+- python3.12
+- dotenv
+- openai
+- mcp
+- rich
+
+## 路线
+1. 配置API KEY和BASE URL实现基本的调用chat功能
+2. 新增console格式输出、实现异步chat
--- augmented/chat_deepseek.py
+++ augmented/chat_deepseek.py
@@ -1,5 +1,7 @@
 import asyncio
 import os
+
+# mcp的工具
 from mcp import Tool
 
 # 使用异步的OpenAI客户端
@@ -8,7 +10,10 @@ from openai import AsyncOpenAI
 # 使用dataclasss来定义数据类
 from dataclasses import dataclass, field
 
+# 定义可被openai 调用的函数
 from openai.types import FunctionDefinition
+
+# 定义聊天消息的参数、工具调用的参数
 from openai.types.chat import (
     ChatCompletionMessageParam,
     ChatCompletionToolParam,
@@ -17,18 +22,39 @@ import dotenv
 from pydantic import BaseModel
 from rich import print as rprint
 
-from augmented.utils import pretty
+from utils import pretty
 
 dotenv.load_dotenv()  # 加载环境变量
+
+
+# 定义工具调用的参数
+class ToolCallFunction(BaseModel):
+    name: str = ""
+    arguments: str = ""
+
+
+# 定义一个返回后工具调用的类，因为只用到id,name和arguments
+class ToolCall(BaseModel):
+    id: str = ""
+    function: ToolCallFunction = ToolCallFunction()
+
+
+# 定义聊天返回的数据类型，content和tool_call调用列表
+class ChatResponse(BaseModel):
+    content: str = ""
+    tool_calls: list[ToolCall] = []
 
 
 @dataclass
 class AsyncChat:
     model: str
     messages: list[ChatCompletionMessageParam] = field(default_factory=list)
+    # 这里面的工具是mcp的工具，类型为Tool
     tools: list[Tool] = field(default_factory=list)
 
-    system_promprt: str = "You are a helpful assistant."
+    # 系统的prompt
+    system_prompt: str = "You are a helpful assistant."
+    # 上下文信息
     context: str = ""
 
     llm: AsyncOpenAI = field(init=False)
@@ -38,5 +64,111 @@ class AsyncChat:
         # 创建异步的OpenAI客户端
         self.llm = AsyncOpenAI(
             api_key=os.environ.get("DEEPSEEK_API_KEY"),
-            base_url=os.environ.get("DEEPSEEK_BASE_URL"),
+            base_url=os.environ.get("DEEPSEEL_BASE_URL"),
         )
+        # 添加系统prompt
+        if self.system_prompt:
+            self.messages.insert(0, {"role": "system", "content": self.system_prompt})
+        # 添加上下文context，提供对话背景
+        if self.context:
+            self.messages.insert(1, {"role": "user", "content": self.context})
+
+    # 将MCP的工具转换为OpenAI的工具定义
+    def getToolsDefinition(self) -> list[ChatCompletionToolParam]:
+        return [
+            ChatCompletionToolParam(
+                type="function",
+                function=FunctionDefinition(
+                    name=tool.name,
+                    description=tool.description,
+                    parameters=tool.parameters,
+                ),
+            )
+            for tool in self.tools
+        ]
+
+    # 异步chat的实现——获取content和tool calls
+    async def chat(self, prompt: str = "", print_llm_output: bool = True):
+        # 输出一下格式
+        pretty.log_title("CHAT")
+
+        # 添加用户输入的prompt
+        if prompt:
+            self.messages.append({"role": "user", "content": prompt})
+
+        print(self.messages)
+
+        # 调用OpenAI的聊天接口，这里用到异步和流式处理
+        chat_kwargs = {
+            "model": self.model,
+            "messages": self.messages,
+            "stream": True,
+        }
+        if self.tools:
+            chat_kwargs["tools"] = self.getToolsDefinition()
+
+        streaming = await self.llm.chat.completions.create(**chat_kwargs)
+
+        # 输出一下格式
+        pretty.log_title("RESPONE")
+        # 处理返回的流式数据，其中tool calls需要单独处理
+        content = ""
+        tool_calls: list[ToolCall] = []
+
+        async for chunk in streaming:
+            delta = chunk.choices[0].delta
+            # 如果是 content
+            if delta.content:
+                content += delta.content or ""
+                if print_llm_output:
+                    print(delta.content, end="", flush=True)
+            # 如果是工具调用
+            if delta.tool_calls:
+                for tool_call_chunk in delta.tool_calls:
+                    # 收到一个tool_call，因为流式传输是片段，所以根据index来判断是否需要创建一个新的tool_call
+                    if len(tool_calls) <= tool_call_chunk.index:
+                        tool_calls.append(ToolCall())
+                    current_tool_call = tool_calls[tool_call_chunk.index]
+                    if tool_call_chunk.id:
+                        current_tool_call.id = tool_call_chunk.id
+                    if tool_call_chunk.function:
+                        current_tool_call.function.name = (
+                            tool_call_chunk.function.name or ""
+                        )
+                        current_tool_call.function.arguments = (
+                            tool_call_chunk.function.arguments or ""
+                        )
+
+        # 维护一下messages
+        self.messages.append(
+            {
+                "role": "assistant",
+                "content": content,
+                "tool_calls": [
+                    {
+                        "type": "function",
+                        "id": tool_call.id,
+                        "function": {
+                            "name": tool_call.function.name,
+                            "arguments": tool_call.function.arguments,
+                        },
+                    }
+                    for tool_call in tool_calls
+                ],
+            }
+        )
+
+        # 返回获取的内容
+        return ChatResponse(content=content, tool_calls=tool_calls)
+
+
+async def example():
+    llm = AsyncChat(
+        model="deepseek-chat",
+    )
+    chat_response = await llm.chat(prompt="Hello")
+    rprint(chat_response)
+
+
+if __name__ == "__main__":
+    asyncio.run(example())
